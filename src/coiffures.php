a155a8d3ee3613a5117fb10174da1814ebaa17c3
SophieWright <swright31@hotmail.ccom>
2021-03-18 12:19:41 +0100

    created file coiffures.php array

---
+++ src/coiffures.php
@@ -0,0 +1,40 @@
+<?php
+$hairstyles = [
+    [
+        'name' => 'hairstyle 1',
+        'image' => 'img src ="https://picsum.photos/250/100?random="',
+        'description' => 'blablabla1',
+        'hairlength' => 'long hair',
+        'price' => '40€',
+        'time' => '45min',
+        'new' => true,
+    ],
+    [
+        'name' => 'hairstyle 2',
+        'image' => 'img src ="https://picsum.photos/250/100?random="',
+        'description' => 'blablabla2',
+        'hairlength' => 'long hair',
+        'price' => '40€',
+        'time' => '45min', 
+        'new' => true,
+    ],
+    [
+        'name' => 'hairstyle 3',
+        'image' => 'img src ="https://picsum.photos/250/100?random="',
+        'description' => 'blablabla3',
+        'hairlength' => 'long hair',
+        'price' => '40€',
+        'time' => '45min',
+        'new' => false,
+    ],
+    [
+        'name' => 'hairstyle 4',
+        'image' => 'img src ="https://picsum.photos/250/100?random="',
+        'description' => 'blablabla4',
+        'hairlength' => 'long hair',
+        'price' => '40€',
+        'time' => '45min', 
+        'new' => false,
+    ],
+]
+?>

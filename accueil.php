073b55fe7952a7478659bd54cbbcc677354bde87
Julien Paillassa <julien-paillassa_student2021@wilder.school>
2021-03-19 10:23:58 +0100

    modif validé

---
+++ accueil.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Le Petit Vintage</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <div class="background-image"></div>
+  <div class="content"></div>
+
+ <section class="enseigne">
+     <h1><img src="LOGO.png" width="60%" height="60%"/></h1>
+ </section>
+
+
+  <button type="button" name="closeMenu">X</button>
+
+ <section class="menu">
+     <h2>Menu</h2>
+ </section>
+
+ <section class="navigation">
+  <ul>
+    <div class="Who">
+      <li><a href="qui-sommes-nous.php">Qui sommes nous ?</a></li>
+    </div>
+    <div class="What">
+      <li><a href="coiffures.php">Nos coiffures</a></li>
+    </div>
+    <div class="How">
+      <li><a href="contact.php">Tarif/contact</a></li>
+    </div>
+    <div class="RDV">
+      <li><a href="rdv.php">Prenez rendez-vous !</a></li>
+    </div>
+  </ul>
+ </section>
+
+
+</body>
+</html>

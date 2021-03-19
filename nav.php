f33504aebda27b06ae3b0620e7ad008dd2dad0c6
Magalien1 <emma.baldovino2@gmail.com>
2021-03-19 15:22:40 +0100

    nav modif

--- nav.php
+++ nav.php
@@ -5,8 +5,8 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link type="text/css" rel="stylesheet" href="style.css">
-    <link type="text/css" rel="stylesheet" href="stylenav.css">
+    
+    <link type="text/css" rel="stylesheet" href="src/stylenav.css">
     <title>Nav</title>
 
 </head>

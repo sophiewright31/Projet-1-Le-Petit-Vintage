93b5f45fa6b6a20e21b677b2a1cb77b906a9163a
Sophie Wright <sophiewright@MacBook-Pro-de-Sophie.local>
2021-03-24 14:12:39 +0100

    end of contact et hairstyle css

--- contactscript.js
+++
@@ -1,9 +0,0 @@
-const message =
-  "Merci d'avoir pris contact avec Le Petit Vintage! Votre message a bien été transmis, notre équipe va vous répondre rapidement.";
-
-document
-  .getElementById("contactForm")
-  .addEventListener("submit", function(event) {
-    event.preventDefault();
-    alert(message);
-  });

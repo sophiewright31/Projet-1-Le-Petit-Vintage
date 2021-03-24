289a9baee731e66ba5304d5e0accca3f84285dfe
Magalien1 <emma.baldovino2@gmail.com>
2021-03-24 10:37:45 +0100

    creation of footer and modifications on nav

--- footer.php
+++ footer.php
@@ -5,12 +5,34 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link type="text/css" rel="stylesheet" href="styles.css">
+    <link type="text/css" rel="stylesheet" href="src/styles/stylefooter.css">
     <title>Footer</title>
 </head>
 <body>
 <section class="footer">
     <div class ="footer-content">
+        <div class="footer-bar"> Le Petit Vintage 2021 - Tout droits réservés - <a class="links" href=""> mentions légales </a></div>
+
+        <div class="footer-bar" >Horaires
+        <ul>
+            <li> du luni au vendredi: 9h00 - 18h00</li>
+            <li> samedi: 9h00 - 18h00</li>
+            <li> dimanche: fermé </li>
+        </ul>
+
+
+        </div>
+
+
+        <div class="contact-footer"> Contactez nous :
+
+            <ul>Horaires
+                <li> 7 Avenue Thiers, 33100 Bordeaux </li>
+                <li><a class ="links" href="mailto:contact@lepetitvintage.fr"> conatct@lepetitvintage.fr </a> </li>
+                <li> 06 12 31 51 09 </li>
+            </ul>
+
+        </div>
 
     </div>
 </section>

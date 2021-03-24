bf70d57e9482b17e0e4d0fa5d1fc2de5bae599e1
Magalien1 <emma.baldovino2@gmail.com>
2021-03-24 09:05:02 +0100

    get js interaction on the button

--- nav.php
+++ nav.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    
+
     <link type="text/css" rel="stylesheet" href="src/stylenav.css">
     <title>Nav</title>
 
@@ -20,24 +20,26 @@
 
  </div>
 
- <div class="burger-menu"> <span> </span></div>
-
 <div>
-    <nav class="content">
+    <nav id="content" >
 
         <ul class="navStyle">
-            <li class="button"> <a href=""> Qui sommes nous ? </a> </li>
-            <li class="button"> <a href="">Nos Coiffures </a> </li>
-            <li class="button"> <a href="">Prendre Rendez-vous </a> </li>
-            <li class="button"> <a href=""> Tarif et Contact </a> </li>
+            <li class="button"> <a href="http://localhost:8000/nav.php"> Qui sommes nous ? </a> </li>
+            <li class="button"> <a href="http://localhost:8000/hairstyles.php">Nos Coiffures </a> </li>
+            <li class="button"> <a href="http://localhost:8000/rdv.php">Prendre Rendez-vous </a> </li>
+            <li class="button"> <a href="http://localhost:8000/contact.php"> Tarif et Contact </a> </li>
         </ul>
 
     </nav>
 
 </div>
-
+    <div class="burger-menu" id="" onclick=" menuToggle() " > <span> </span></div>
 
 </section>
-<script src="script.js"></script>
+
+<script src="scriptnav.js">
+
+</script>
+
 </body>
 </html>
---
+++ scriptnav.js
@@ -0,0 +1,12 @@
+
+
+function menuToggle(){
+    let nav = document.getElementById('content');
+    nav.classList.toggle('active');
+
+}
+
+function bgmenu() {
+    let menu = document.getElementsByClassName('burger-menu');
+    menu.classList.toggle('active');
+}

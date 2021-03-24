b1a483c0fcc5537491c52072aa9ec5e20e3f3a91
Magalien1 <emma.baldovino2@gmail.com>
2021-03-24 15:09:55 +0100

    change before checkpoint 3

--- nav.php
+++ nav.php
@@ -1,16 +1,4 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <link type="text/css" rel="stylesheet" href="src/stylenav.css">
-    <title>Nav</title>
-
-</head>
-<body>
 
 <section >
 <div class="header">
@@ -24,7 +12,7 @@
     <nav id="content" >
 
         <ul class="navStyle">
-            <li class="button"> <a href="http://localhost:8000/nav.php"> Qui sommes nous ? </a> </li>
+            <li class="button"> <a href="http://localhost:8000/whoRwe.php"> Qui sommes nous ? </a> </li>
             <li class="button"> <a href="http://localhost:8000/hairstyles.php">Nos Coiffures </a> </li>
             <li class="button"> <a href="http://localhost:8000/rdv.php">Prendre Rendez-vous </a> </li>
             <li class="button"> <a href="http://localhost:8000/contact.php"> Tarif et Contact </a> </li>
@@ -41,5 +29,3 @@
 
 </script>
 
-</body>
-</html>
